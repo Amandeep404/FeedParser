e50d478ecf87c50f0b41b462d1511278f9fc6b83
Amandeep Singh <contactamandeep18@gmail.com>
2024-04-23 16:12:03 +0530

    revamp the ui

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
+from app_config import app_config
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///feederDatabase.db'
-app.secret_key = 'secret_key'
+app.secret_key = app_config.get('FLASK_SECRET_KEY')
