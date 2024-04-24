1f426f0c758b37edd7e1523c9635cf3759eae604
Amandeep Singh <contactamandeep18@gmail.com>
2024-04-24 20:30:02 +0530

    solved sidebar not fully  collapsing error

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,6 +1,7 @@
 const sidebar = document.getElementById("sidebar");
 const sidebarToggle = document.getElementById("sidebar-toggle");
 const sidebarContent = document.getElementById("sidebar-content");
+const username = document.getElementById("user-name");
 const mainContent = document.getElementById("main-content");
 const toggleIcon = document.getElementById("toggle-icon");
 
@@ -9,12 +10,14 @@ sidebarToggle.addEventListener("click", function () {
     // Expanded
     sidebar.classList.replace("w-1/6", "w-12"); // Collapse
     sidebarContent.classList.add("hidden"); // Hide content
+    username.classList.add("hidden"); // Hide content
     toggleIcon.src = "/static/images/sidebar_close.png"; // Change to collapse icon
     mainContent.classList.add("pl-12"); // Adjust for collapsed sidebar
   } else {
     // Collapsed
     sidebar.classList.replace("w-12", "w-1/6"); // Expand
     sidebarContent.classList.remove("hidden"); // Show content
+    username.classList.remove("hidden"); // Show content
     toggleIcon.src = "/static/images/sidebar_open.png"; // Change to open icon
     mainContent.classList.remove("pl-12"); // Adjust for expanded sidebar
   }
