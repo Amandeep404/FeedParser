26fb1dd3e6a6a255d633ec9ebb0b1ba0969dd970
Amandeep Singh <contactamandeep18@gmail.com>
2024-05-06 16:12:14 +0530

    made safety changes

--- app_config.py
+++
@@ -1,8 +0,0 @@
-app_config = {
-    'OAUTH2_CLIENT_ID':'359090012761-arl7t3sk750kv7pnj7va26919622434r.apps.googleusercontent.com',
-    'OAUTH2_CLIENT_SECRET':'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',
-    'OAUTH2_META_URL':'https://accounts.google.com/.well-known/openid-configuration',
-    'FLASK_SECRET_KEY':'hello',
-    'FLASK_PORT':5000
-    
-}
